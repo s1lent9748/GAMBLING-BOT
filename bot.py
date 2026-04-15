f3464f9d418dc4bab217403447fe31ce90aa4828
GRACEEE <aidenadams9748@gmail.com>
2026-04-14 23:01:56 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 # ============================================================
 #  CONFIG — edit these, or set BOT_TOKEN as an env variable
 # ============================================================
-BOT_TOKEN     = os.getenv("BOT_TOKEN", "MTQ5MzcyMjA1MTQ0MDI4Mzc0OQ.GiRlwN.-lqEUmDLAODcvaNVtxgu8BAI63fOVciinw0CnM")
+BOT_TOKEN     = os.getenv("BOT_TOKEN", "MTQ5MzcyMjA1MTQ0MDI4Mzc0OQ.Gd0ihm.ZRxHbjLGh2SepHzBSk69FGMx2OhdQ-r65JkEsM")
 OWNER_ID      = 1335801328516993076      # your Discord user ID (integer)
 PREFIX        = "!"
 CURRENCY      = "🪙"
